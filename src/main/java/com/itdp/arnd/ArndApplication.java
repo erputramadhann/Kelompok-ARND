ae9f76081481181e260923b0269d630c52030c3c
Ridwan Ramadhan <erputramadhan@gmail.com>
2025-03-01 10:36:24 +0700

    first commit

---
+++ src/main/java/com/itdp/arnd/ArndApplication.java
@@ -0,0 +1,13 @@
+package com.itdp.arnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArndApplication.class, args);
+	}
+
+}
